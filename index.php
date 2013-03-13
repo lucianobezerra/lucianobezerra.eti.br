66f87865d599c57df8161869159a93ce16021658
Luciano Bezerra <contato@lucianobezerra.eti.br>
2013-03-13 11:32:11 -0300

    pagina inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Luciano Bezerra</title>
+    </head>
+    <body>
+        <h2>Meus Contatos</h2>
+        <p>Email: contato@lucianobezerra.eti.br</p>
+        <p>Twitter: @lucianobezerra</p>
+        <p>Facebook: facebook.com/luciano.bezerra</p>
+        <p>Orkut: Essa palavra não existe no meu dicionário</p>
+    </body>
+</html>
